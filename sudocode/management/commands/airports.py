da1916594a38ff98235321ea6f8e18690e55c000
CodeRed <codered@tohka.us>
2018-11-03 20:21:02 +0100

    json: Better airport data

--- sudocode/management/commands/airports.py
+++ sudocode/management/commands/airports.py
@@ -7,19 +7,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         Airport.objects.all().delete()
-        airports = requests.get('https://raw.githubusercontent.com/jbrooksuk/JSON-Airports/master/airports.json', headers={'User-Agent': 'Mozilla/5.0'}).json()
+        airports = requests.get('https://raw.githubusercontent.com/mwgg/Airports/master/airports.json', headers={'User-Agent': 'Mozilla/5.0'}).json()
 
-        for obj in airports:
-            if obj['type'] != 'airport' or not obj['name']:
+        for key, obj in airports.items():
+            if not obj['name'] or not obj['city'] or not obj['iata'] or obj['iata'].isdigit():
                 continue
 
             airport = Airport()
-            airport.code = obj['iata']
-            airport.iso_code = obj['iso']
-            airport.name = obj['name']
+            airport.code = obj['iata'].strip()
+            airport.name = obj['name'].strip()
+            airport.city = obj['city'].strip()
             airport.latitude = float(obj.get('lat', 0))
             airport.longitude = float(obj.get('lon', 0))
-            airport.continent = obj['continent']
+            airport.country = obj['country'].strip()
             airport.save()
 
         print('Added {0} airports.'.format(Airport.objects.count()))
--- sudocode/models.py
+++ sudocode/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Airport(models.Model):
     code = models.CharField(max_length=20, primary_key=True)
-    iso_code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
     latitude = models.DecimalField(max_digits=10, decimal_places=6)
     longitude = models.DecimalField(max_digits=10, decimal_places=6)
-    continent = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
